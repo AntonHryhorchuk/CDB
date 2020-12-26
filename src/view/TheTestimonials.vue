033e198ecca798ec4ce81a1ee3036fbfb70057e4
AntonHryhorchuk <hryhorchukanton@gmail.com>
2020-12-26 19:55:45 +0200

    css for carousel

--- src/view/TheTestimonials.vue
+++ src/view/TheTestimonials.vue
@@ -71,15 +71,8 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import "swiper/css/swiper.css";
+
 export default {
-  name: "swiper-example-multiple-slides-per-biew",
-  title: "Multiple slides per view",
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
   data() {
     return {
       items: [
